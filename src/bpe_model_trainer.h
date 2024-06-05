2dd4a0c3617c93986921a6bc236abcbaa9663599
Burhanuddin Limdi <burhanuddinlimdiwala5123@gmail.com>
2024-06-05 19:46:25 +0530

    code updated

--- src/bpe_model_trainer.h
+++ src/bpe_model_trainer.h
@@ -24,7 +24,7 @@
 #include "third_party/absl/container/btree_set.h"
 #include "third_party/absl/container/flat_hash_map.h"
 #include "trainer_interface.h"
-#include <descriptor.h>
+#include "descriptor.h"
 
 namespace sentencepiece
 {
